1fcafe01ee96c7927100cd6b215c95febf1e6927
mark <78883486+MarkBrooks3@users.noreply.github.com>
2026-04-19 21:04:10 -0500

    added option for user to change size of grid

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 const container = document.querySelector('#container');
-let squares = 20 * 20;
+const button = document.querySelector('button');
 
-for (let i = 0; i < squares; i++) {
-  let square = document.createElement('div');
-  square.style.width = `${100 / Math.sqrt(squares)}%`;
-  square.classList.add('square');
-  container.appendChild(square);
+let squares = 16;
 
-  square.addEventListener('mouseover', () => {
-    square.classList.toggle('color');
+function createGrid(size) {
+  size *= size;
+  for (let i = 0; i < size; i++) {
+    let square = document.createElement('div');
+    square.style.width = `${100 / Math.sqrt(size)}%`;
+    square.classList.add('square');
+    container.appendChild(square);
+
+    square.addEventListener('mouseover', () => {
+      square.classList.toggle('color');
+    });
+  }
+}
+
+function deleteGrid() {
+  let divs = container.querySelectorAll('div');
+  divs.forEach((div) => {
+    container.removeChild(div);
   });
 }
+
+createGrid(squares);
+
+button.addEventListener('click', () => {
+  const input = parseInt(document.querySelector('input').value, 10);
+  console.log(typeof input);
+  if (!input || input < 1 || input > 100) {
+    alert('please enter a number between 1 and 100');
+  } else {
+    deleteGrid();
+    createGrid(input);
+  }
+});
